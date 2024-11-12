66736d97c9e0d2fed70d3dd8dbb424bb77a9b189
OXeFeN <lukasen14@atlas.cz>
2024-11-12 20:49:45 +0100

    Lekce 8.2
    Object practice

--- Lekce 8.2.py
+++ Lekce 8.2.py
@@ -9,10 +9,25 @@ class Car:
     def start(self):
         print(f"Startuji motor {self.engine}")
 
+class House:
+    def __init__(self, roof, walls, chimney, windows):
+        self.roof = roof
+        self.walls = walls
+        self.chimney = chimney
+        self.windows = windows
+
+    def intruduce(self):
+        print(f"My roof is {self.roof}, walls are {self.walls} with {self.windows} windows and {self.chimney} chimney")
+
 
 c1 = Car(engine="2.0 CRDI", wheels=4, color="Red")
 c2 = Car(engine="3.0 TDI", wheels=4, color="Blue")
+b1 = House(roof="sloped roof", walls="brick", windows=5, chimney=True)
+b2 = House(roof="flat roof", walls="concrete", windows=10, chimney=False)
 
 print(c1.engine, c1.color)
 
 c1.start()
+
+b1.intruduce()
+b2.intruduce()
