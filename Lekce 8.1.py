38793fbc9f9f62908220a0366770a266c27c0889
OXeFeN <lukasen14@atlas.cz>
2024-11-12 19:58:57 +0100

    Task 5
    complete

--- Lekce 8.1.py
+++ Lekce 8.1.py
@@ -1,6 +1,6 @@
 
 file_path = r"C:\Lukas\Python\Domácí úkoly\HW-8\HW-8-PY-2024\random_nouns.txt"
-file_path2 = r"C:\Lukas\Python\Domácí úkoly\HW-8\HW-8-PY-2024\star_nouns.txt"
+file_path2 = r"C:\Lukas\Python\Domácí úkoly\HW-8\HW-8-PY-2024\filter_nouns.txt"
 
 #Task 1
 def task1():
@@ -38,18 +38,21 @@ def task3():
     
     with open(file_path2, "w") as f:
        f.writelines(word for word in words)
-    print(words)
-    print("-" * 50)
 
 #Task 4
-
 def task4():
-    words = []
-    stars = "*" * 12
-
     with open(file_path, "r") as source, open(file_path2, "w") as new:
         words = (source.read()).split()
-        words.append(stars)
+        words.append("*" * 12)
         new.writelines(word + "\n" for word in words)
 
-task4()
+#Task 5
+def task5():
+    with open(file_path, "r") as source, open(file_path2, "w") as new:
+        words = (source.read()).split()
+        user = str(input("Set a character for the filter: ").strip().lower())
+        for word in words:
+            if word.startswith(user[0]):
+                new.writelines(word + "\n")
+
+task5()
