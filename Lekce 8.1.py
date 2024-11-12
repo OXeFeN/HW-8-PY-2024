ea7c6f4000ad6790f94fd38948a89ddb08d553d6
OXeFeN <lukasen14@atlas.cz>
2024-11-12 19:46:50 +0100

    Task 4
    complete

--- Lekce 8.1.py
+++ Lekce 8.1.py
@@ -1,22 +1,21 @@
 
 file_path = r"C:\Lukas\Python\Domácí úkoly\HW-8\HW-8-PY-2024\random_nouns.txt"
-file_path2 = r"C:\Lukas\Python\Domácí úkoly\HW-8\HW-8-PY-2024\rev_nouns.txt"
+file_path2 = r"C:\Lukas\Python\Domácí úkoly\HW-8\HW-8-PY-2024\star_nouns.txt"
 
 #Task 1
 def task1():
-    with open(file_path, "r") as f:
-        text = f.read()
+    with open(file_path, "r") as source, open(file_path, "w") as new:
+        text = source.read()
 
-    words = []
-    seven_words = []
+        words = []
+        seven_words = []
 
-    words = text.split()
-    for word in words:
-        if len(word) >= 7:
-            seven_words.append(word)
+        words = text.split()
+        for word in words:
+            if len(word) >= 7:
+                seven_words.append(word)
 
-    with open(file_path, "w") as f:
-        f.writelines(word + "\n" for word in words)
+        new.writelines(word + "\n" for word in words)
 
 #Task 2
 def task2():
@@ -48,11 +47,9 @@ def task4():
     words = []
     stars = "*" * 12
 
-    with open(file_path, "r") as f:
-        words = f.readlines()
-    
-
-
-    print(stars)
+    with open(file_path, "r") as source, open(file_path2, "w") as new:
+        words = (source.read()).split()
+        words.append(stars)
+        new.writelines(word + "\n" for word in words)
 
 task4()
